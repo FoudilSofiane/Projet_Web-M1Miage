fe8e9f8856e32a1216d08a242bc6327092d06d33
Sofiane FOUDIL <sofiane.foudil@outlook.com>
2019-11-11 11:08:46 +0100

    mongoose commit

---
+++ server.js
@@ -0,0 +1,29 @@
+if(process.env.NODE_ENV !== 'production') { //vérifier si on est dans l'environnement de prod
+    require('dotenv').config() //va charger les variables du fichier .env dans la variable process.env si on est dans l'environnement de développement
+}
+
+const express = require('express') //importer la librairie Express
+const app = express()
+const expressLayouts = require('express-ejs-layouts') //importer le package avec les layouts
+
+const indexRouter = require('./routes/index') //va récupérer infos à partir du fichier index dans le dossier routes
+
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views') //vues issues du dossier views
+app.set('layout', 'layouts/layout') //layouts issus du dossier layout
+app.use(expressLayouts)
+app.use(express.static('public')) //style, images, JavaScript
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true}) //connnexion au serveur, option config MongoDB
+const db = mongoose.connection
+db.on('Erreur lors de la connexion', error => console.error(error))
+db.once('open', () => console.log('Connecté à Mongoose'))
+
+
+app.use('/', indexRouter)
+
+
+app.listen(process.env.PORT || 3000) //le serveur va écouter sur le port 3000
+
